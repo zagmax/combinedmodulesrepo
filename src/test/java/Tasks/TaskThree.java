7c959a481ada01a36be3f55321ca989d3822e40a
Maksym Zahreba <zagmax@gmail.com>
2024-01-25 11:44:19 +0200

    finished task 3

--- src/test/java/Tasks/TaskThree.java
+++ src/test/java/Tasks/TaskThree.java
@@ -11,22 +11,21 @@ public class TaskThree {
     @Test
     @Name("1. Verify that allows creating a User")
     public void checkUserCreate() {
-        String body = "{\"id\": 0,\"username\": \"string\",\"firstName\": \"string\",\"lastName\": \"string\",\"email\": \"string\",\"password\": \"string\",\"phone\": \"string\",\"userStatus\": 0}";
-        // var respo = given().body(body).when().auth().basic("user","pass").post(endp).then();//get(endp + "/login?username=test&password=test").then().statusCode(200);
-        //respo.log().body();
-        var response = given().body(body).when().post(endp + "user").then();//.statusCode(200);
-        response.log().body();
-    }
-/*
+        String body = """
+                {
                   "id": 0,
-                  "username": "TheUserName",
-                  "firstName": "FiName",
-                  "lastName": "LaName",
+                  "username": "UNAME",
+                  "firstName": "FNAME",
+                  "lastName": "LNAME",
                   "email": "mail@mail.ma",
                   "password": "passwordsss",
-                  "phone": "+380111111111",
+                  "phone": "+410101010",
                   "userStatus": 0
-*/
+                }
+                """;
+        var response = given().header("Content-type", "application/json").and().body(body).when().post(endp + "user").then().statusCode(200);
+        response.log().body();
+    }
 
     @Test
     @Name("2. Verify that allows login as a User")
@@ -42,19 +41,28 @@ public class TaskThree {
                 [
                 {
                 "id": 0,
-                "username": "string",
-                "firstName": "string",
-                "lastName": "string",
-                "email": "string",
-                "password": "string",
-                "phone": "string",
+                "username": "string0",
+                "firstName": "string0",
+                "lastName": "string0",
+                "email": "string0",
+                "password": "string0",
+                "phone": "string0",
                 "userStatus": 0
+                }
+                ,
+                {
+                "id": 1,
+                "username": "string1",
+                "firstName": "string1",
+                "lastName": "string1",
+                "email": "string1",
+                "password": "string1",
+                "phone": "string1",
+                "userStatus": 1
                 }
                 ]
                 """;
-        // var respo = given().body(body).when().auth().basic("user","pass").post(endp).then();//get(endp + "/login?username=test&password=test").then().statusCode(200);
-        //respo.log().body();
-        var response = given().body(body).when().post(endp + "user/createWithArray").then();//.statusCode(200);
+        var response = given().header("Content-type", "application/json").and().body(body).when().post(endp + "user/createWithList").then().statusCode(200);
         response.log().body();
     }
 
@@ -68,16 +76,14 @@ public class TaskThree {
     @Test
     @Name("5. Verify that allows adding a new Pet")
     public void checkAddingPet() {
-        String head = "accept: application/json";
-        String head2 = "Content-Type: application/json";
         String body = """
                 {
-                  "id": 0,
+                  "id": 10,
                   "category": {
-                    "id": 0,
+                    "id": 1,
                     "name": "string"
                   },
-                  "name": "doggie",
+                  "name": "NEW DOG",
                   "photoUrls": [
                     "string"
                   ],
@@ -90,24 +96,69 @@ public class TaskThree {
                   "status": "available"
                 }
                 """;
-        var response = given().body(body).when().post("https://petstore.swagger.io/v2/pet").then().log().body();//.statusCode(200);
-        //response.log().body();
+        var response = given().header("Content-type", "application/json").and().body(body).when().post(endp + "pet").then().statusCode(200);
+        response.log().body();
     }
 
     @Test
     @Name("6. Verify that allows updating Pet’s image")
     public void checkAddPetImage() {
+        String body = """
+                {
+                "id": 5,
+                "category": {
+                  "id": 0,
+                  "name": "dog"
+                },
+                "name": "Doggo the test",
+                "photoUrls": [
+                  "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg"
+                ],
+                "tags": [
+                  {
+                    "id": 0,
+                    "name": "string"
+                  }
+                ],
+                "status": "OUT OF STOCK"
+                }
+                """;
+        var response = given().header("Content-type", "application/json").and().body(body).when().put(endp + "pet").then().statusCode(200);
+        response.log().body();
     }
 
     @Test
     @Name("7. Verify that allows updating Pet’s name and status")
     public void checkUpdatePetNameStatus() {
+        String body = """
+                {
+                  "id": 0,
+                  "category": {
+                    "id": 0,
+                    "name": "string"
+                  },
+                  "name": "doggieNOT",
+                  "photoUrls": [
+                    "string"
+                  ],
+                  "tags": [
+                    {
+                      "id": 0,
+                      "name": "string"
+                    }
+                  ],
+                  "status": "OUT OF STOCK"
+                }
+                """;
+        var response = given().header("Content-type", "application/json").and().body(body).when().put(endp + "pet").then().statusCode(200);
+        response.log().body();
     }
 
     @Test
     @Name("8. Verify that allows deleting Pet ")
     public void checkDeletePet() {
-        var response = given().when().delete(endp + "pet/8").then().statusCode(200);
+        checkAddingPet();
+        var response = given().when().delete(endp + "pet/10").then().statusCode(200);
         response.log().body();
     }
 
